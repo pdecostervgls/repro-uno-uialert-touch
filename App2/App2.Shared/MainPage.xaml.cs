c6802a87b8d8cbf3aafce5327abfff2896c472e5
Patrick M. Decoster <p.decoster@vogels.com>
2019-06-27 13:30:20 +0200

    Initial repro

---
+++ App2/App2.Shared/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Acr.UserDialogs;
+using UIKit;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+#if __IOS__
+            var alert = UIAlertController.Create("This is a title", "This is a message", UIAlertControllerStyle.Alert);
+            alert.AddAction(UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, null));
+            alert.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+            alert.AddTextField((tf) =>
+            {
+                tf.Placeholder = "placeholder";
+            });
+            UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(alert, true, null);
+            //await UserDialogs.Instance.PromptAsync("This is a message", "This is a title", "OK", "Cancel", "Placeholder");
+#endif
+        }
+    }
+}
